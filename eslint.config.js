cf63f624a555921032eed1b992c4a3bd8f364eb4
juan0p123 <juanoportus@alu.unach.cl>
2026-04-07 09:42:04 -0400

    Arreglo de errores en la configuracion de ESLINT

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,23 @@
-const js = require('@eslint/js');
+﻿const js = require('@eslint/js');
 
 module.exports = [
   js.configs.recommended,
   {
+    files: ['calculadora.js', 'index.html'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        alert: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+      }
+    },
+    rules: {}
+  },
+  {
+    files: ['app.js', 'tests/**/*.js', 'eslint.config.js'],
     languageOptions: {
       ecmaVersion: 2021,
       globals: {
